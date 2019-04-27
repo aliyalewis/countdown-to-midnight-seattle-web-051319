988e53d0db0d6f49be95ac6dbffaadc2595baf2f
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-27 01:58:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
-
-def countdown
+def countdown(i)
+  while countdown > 0
+    puts "#{i) SECOND(S)!}"
+    i -= 1
+    break if i = 0
+    puts "HAPPY NEW YEAR!"
+    end
+  end
 end
